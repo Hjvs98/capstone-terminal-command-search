3003148fefaaebcef30a38d900b3a9cc4c3c5882
Hunter Saunders <hjvs98@gmail.com>
2022-06-28 21:42:32 -0600

    fixed path.join location

--- server/index.js
+++ server/index.js
@@ -7,10 +7,10 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "../client/index.html"));
 });
 app.get("/styles", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/index.css"));
+  res.sendFile(path.join(__dirname, "../client/index.css"));
 });
 
 const port = process.env.PORT || 3005;
