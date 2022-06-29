9268d76f0b03e7437aae8c7d3e006590b36933f1
Hunter Saunders <hjvs98@gmail.com>
2022-06-28 21:36:12 -0600

    initial commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+app.get("/styles", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.css"));
+});
+
+const port = process.env.PORT || 3005;
+
+app.listen(port, () => {
+  console.log(`I'll be right by your side, 'til ${port}`);
+});
