a04341e87f5e25e75a1bf9b46560cd5a5d2f0794
Hunter Saunders <hjvs98@gmail.com>
2022-07-06 09:43:04 -0600

    reverted seeding

--- server/seed.js
+++ server/seed.js
@@ -1,6 +1,6 @@
-require("dotenv").config();
-const { CONNECTION_STRING } = process.env;
-const Sequelize = require("sequelize");
+// require("dotenv").config();
+// const { CONNECTION_STRING } = process.env;
+// const Sequelize = require("sequelize");
 
 // you wouldn't want to rejectUnauthorized in a production app, but it's great for practice
 // const sequelize = new Sequelize(CONNECTION_STRING, {
