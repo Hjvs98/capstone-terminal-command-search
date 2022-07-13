93d0ad74e72e10632e3b1f0c391d076fe5c81ea1
Hunter Saunders <hjvs98@gmail.com>
2022-07-12 18:54:30 -0600

    deleted images and did some other stuff

--- server/index.js
+++ server/index.js
@@ -6,13 +6,27 @@ const { getPage } = require("./controller");
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+//////////////////////////////////
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../client/home.html"));
 });
 app.get("/styles", function (req, res) {
   res.sendFile(path.join(__dirname, "../client/index.css"));
 });
+///////////////////////////////////
+const {
+  getCommands,
+  deleteCommand,
+  createCommand,
+  updateCommand,
+} = require("./controller");
+
+app.get(`/api/commands`, getCommands);
+app.delete(`/api/commands/:id`, deleteCommand);
+app.post(`/api/commands`, createCommand);
+app.put(`/api/commands/:id`, updateCommand);
+///////////////////////////
+
 // app.get("/byweek", function (req, res) {
 //   res.sendFile(path.join(__dirname, "../client/byweek.html"));
 // });
--- server/seed.js
+++ server/seed.js
@@ -1,12 +1,41 @@
-// require("dotenv").config();
-// const { CONNECTION_STRING } = process.env;
-// const Sequelize = require("sequelize");
+require("dotenv").config();
+const { CONNECTION_STRING } = process.env;
+const Sequelize = require("sequelize");
 
-// const sequelize = new Sequelize(CONNECTION_STRING, {
-//   dialect: "postgres",
-//   dialectOptions: {
-//     ssl: {
-//       rejectUnauthorized: false,
-//     },
-//   },
-// });
+const sequelize = new Sequelize(CONNECTION_STRING, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+module.exports = {
+  seed: (req, res) => {
+    sequelize
+      .query(
+        `
+        create table commands (
+            command_id serial primary key, 
+            command varchar(300),
+            command_name varchar(100),
+            command_description varchar(1000),
+            week integer,
+            subject varchar(100) ,
+            required_for_code_to_function boolean,
+            required_parameters_or_arguments varchar(300),
+            optional_parameters varchar(300),
+        );
+        insert into commands (command, command_name, command_description, week, subject, required_for_code_to_function, required_parameters_or_arguments, optional_parameters)
+        values ('ls', 'list', 'short for list, shows all the files/folder where you currently are.', '1', "file management", false, "nothing", "nothing"),
+         ('cd <required parameter>', 'change directory', 'go to a different directory or file', '1', "file management", true, "what file or directory you want to navigate to", "nothing");
+       `
+      )
+      .then(() => {
+        console.log("DB seeded!");
+        res.sendStatus(200);
+      })
+      .catc((err) => console.log("error seeding DB", err));
+  },
+};
